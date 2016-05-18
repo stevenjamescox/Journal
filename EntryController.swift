4ca5c3f06d435f12b871762256275d9db7db9042
Steve Cox <coxsteve@gmail.com>
2016-05-18 14:47:40 -0600

    entry model started

---
+++ EntryController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Steve Cox on 5/18/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Entry.swift
@@ -0,0 +1,32 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Steve Cox on 5/18/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Entry: Equatable {
+    
+    let timestamp: NSDate
+    let title: String
+    let body: String
+    
+    init(timestamp: NSDate, title: String, body: String){
+    self.timestamp = timestamp
+    self.title = title
+    self.body = body
+    
+    }
+}
+
+func ==(lhs: Entry, rhs: Entry) -> Bool {
+    return lhs.timestamp == rhs.timestamp && lhs.title == rhs.title && rhs.body == rhs.body
+    
+}
+
+
+
+
